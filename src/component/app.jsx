c49c8d671c4beb0e30b2818fbfcd011525cbc7b3
anwarraja26 <anwarraja.r2023ai-ds@sece.ac.in>
2024-11-08 23:00:43 +0530

    Initial commit

---
+++ src/component/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./header.jsx";
+import Body from "./body.jsx";
+import Footer from "./footer.jsx";
+
+function App(){
+    return(
+        <div>
+           
+        </div>
+    );
+}
+export default App;
